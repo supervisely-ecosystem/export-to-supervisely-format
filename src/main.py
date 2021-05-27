266a6f2988460dccb26cf36c160079610e887fea
DVMix <dvmixalkin@mail.ru>
2021-05-27 17:48:58 +0300

    .

--- src/main.py
+++ src/main.py
@@ -16,6 +16,34 @@ def download_as_sly(api: sly.Api, task_id, context, state, app_logger):
         download_json_plus_images(api, project, dataset_ids)
     else:
         download_only_json(api, project, dataset_ids)
+    full_archive_name = str(project_info.id) + '_' + project_info.name + '.tar'
+    result_archive = os.path.join(my_app.data_dir, full_archive_name)
+    sly.fs.archive_directory(dest_dir, result_archive)
+    app_logger.info("Result directory is archived")
+    
+    download_dir = os.path.join(g.my_app.data_dir, f'{project.id}_{project.name}')
+    
+    full_archive_name = str(project.id) + '_' + project.name + '.tar'
+    result_archive = os.path.join(my_app.data_dir, full_archive_name)
+    sly.fs.archive_directory(download_dir, result_archive)
+    app_logger.info("Result directory is archived")
+
+    upload_progress = []
+    remote_archive_path = "/Download-data/{}_{}".format(task_id, full_archive_name)
+
+
+    def _print_progress(monitor, upload_progress):
+        if len(upload_progress) == 0:
+            upload_progress.append(sly.Progress(message="Upload {!r}".format(full_archive_name),
+                                                total_cnt=monitor.len,
+                                                ext_logger=app_logger,
+                                                is_size=True))
+        upload_progress[0].set_current_value(monitor.bytes_read)
+
+    file_info = api.file.upload(g.TEAM_ID, result_archive, remote_archive_path,
+                                lambda m: _print_progress(m, upload_progress))
+    app_logger.info("Uploaded to Team-Files: {!r}".format(file_info.full_storage_url))
+    api.task.set_output_archive(task_id, file_info.id, full_archive_name, file_url=file_info.full_storage_url)
     g.my_app.stop()
 
 
