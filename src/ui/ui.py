5b3412b45c7824765d4d87791b58ee3eb1aab2d6
DVMix <dvmixalkin@mail.ru>
2021-05-28 10:49:04 +0300

    .rm unused

--- src/ui/ui.py
+++
@@ -1,8 +0,0 @@
-import supervisely_lib as sly
-import globals as g
-
-
-def init(data, state):
-    data['radioButtons'] = g.mode
-    state['radioButtons'] = g.mode
-    state['checked'] = g.replace_method
